f0f012b0419fc2bcb9bb1df126940702da4296f3
Luda Zhao <94luda@gmail.com>
2015-11-17 13:19:25 -0800

    fixed bug with sliders

--- js/slider.js
+++ js/slider.js
@@ -30,24 +30,61 @@
     $( "#slider" ).slider({
       value: 0,
       min: 0,
-      max: 3,
+      max: 4,
       step: 1,
       slide: function( event, ui ) {
         $("#cf7 img").removeClass("opaque");
 
         var newImage = ui.value;
-        console.log(newImage)
+        var oldImage = $("#slider").slider("value");
+        if (newImage == 4) {
+            $("#historical-maps").fadeToggle( "slow", function() {
+                $("#modern-maps").css('display','inline');
+                $("#container_of_slider").before($("#modern-maps"));
+                $("#container_of_slider").css('padding-top', '0px');
+                $( "#amount" ).val( "2015 A.D.(Now!)");
+                //$("#modern-maps").fadeToggle("slow", "linear");
+            });
+        } else if (oldImage == 4) {
+            $("#modern-maps").css('display', "none");
+            $("#modern-maps").before($("#container_of_slider"));
+            $("#container_of_slider").css('padding-top', '120px');
+            $("#historical-maps").fadeToggle( "slow");
+        }
         $("#cf7 img").eq(newImage).addClass("opaque");
 
         $("#cf7_controls span").removeClass("selected");
         $(this).addClass("selected");
 
-        $( "#amount" ).val( ui.value * 100 + 1400 + "A.D. ");
+        $( "#amount" ).val( ui.value * 50 + 1800 + "A.D. ");
       }
     });
-    $( "#amount" ).val( $( "#slider" ).slider( "value" )  * 100 + 1400  + "A.D. ");
+    $( "#amount" ).val( $( "#slider" ).slider( "value" )  * 50 + 1800  + "A.D. ");
   });
 
+           // L.featureLayer(
+           // {
+           //     // this feature is in the GeoJSON format: see geojson.org
+           //     // for the full specification
+           //     type: 'Feature',
+           //     geometry: {
+           //         type: 'Point',
+           //         // coordinates here are in longitude, latitude order because
+           //         // x, y is the standard for GeoJSON and many formats
+           //         coordinates: [
+           //         -39.916721142292,
+           //         116.383371603574 
+           //         ]
+           //     },
+           //     properties: {
+           //         title: 'Example',
+           //         // one can customize markers by adding simplestyle properties
+           //         // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+           //         'marker-size': 'large',
+           //         'marker-color': '#BE9A6B',
+           //         'marker-symbol': 'cafe'
+           //     }
+           // }).addTo(map);
   // With JQuery
 // $('#ex1').slider({
 //   formatter: function(value) {
