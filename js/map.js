c8f30bdb64c1a2649914a420ff1a21a5def7c0e0
Luda Zhao <94luda@gmail.com>
2015-12-03 16:59:55 -0800

    latest changes - Luda

--- js/map.js
+++ js/map.js
@@ -1,17 +1,72 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibHVkYXpoYW8iLCJhIjoiY2loa3h6NWFpMDZrN3Rqajc0eWQ0NnUxZyJ9.80Gv5oagHmAae16mm2E00w';
 // Create a map in the div #map
-L.mapbox.map('map', 'ludazhao.oa0l9b1f').setView([39.9167, 116.3833], 11);
+var map = L.mapbox.map('map', 'ludazhao.oa0l9b1f');
 
-//var map = L.map('map').setView([39.9167, 116.3833], 12);
-//L.tileLayer('http://api.tiles.mapbox.com/v4/examples.map-zr0njcqy/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVha3MxNCIsImEiOiJjaWgxZjIyMHQweDNoNG1tM3huYWRmMWNwIn0.Yf-GrrFm9884nujMx99d1A', {
-//    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//    maxZoom: 18,
-//    id: 'beaks14.o65g20jm',
-//    accessToken: 'pk.eyJ1IjoiYmVha3MxNCIsImEiOiJjaWgxZjIyMHQweDNoNG1tM3huYWRmMWNwIn0.Yf-GrrFm9884nujMx99d1A'
-//}).addTo(map);
-//
-//map.on('click', function(e) {
-//    console.log(e.latlng.toString());
-//});
+map.setView([39.9167, 116.3833], 11);
 
+var popupContent = "<p> Here is Wudaokou! </p><iframe width='560' height='315' src='https://www.youtube.com/embed/rQ8Wb7IRNgA' frameborder='0' allowfullscreen></iframe>";
+
+map.featureLayer.on('ready', function(e) {
+	map.featureLayer.eachLayer(function(marker) {
+	        if (marker.feature.properties.title === 'Wudaokou') {
+	            marker.bindPopup(popupContent,{
+	                minWidth: 580
+	            });
+	        }
+	    });
+});
+
+$('#add_button').click(function(e) {
+	e.stopPropagation();
+    $(this).toggleClass('btn-danger');
+    $(this).toggleClass('active');
+    $(this).toggleClass('btn-primary');
+    $("#add_form_background").toggle();
+    $(this).html() == "Add New Pins" ? $(this).html('Click on Map to Save') : $(this).html('Add New Pins');
+})
+
+$('add_form_background').click(function(e) {
+    e.stopPropagation();
+})
+
+//When the map is clicked, add the new pin
+map.on('click', function(e) {
+	if ($('#add_button').hasClass('active')) {
+		var lat = e.latlng.lat;
+		var lng = e.latlng.lng;
+		L.mapbox.featureLayer({
+		    // this feature is in the GeoJSON format: see geojson.org
+		    // for the full specification
+		    type: 'Feature',
+		    geometry: {
+		        type: 'Point',
+		        // coordinates here are in longitude, latitude order because
+		        // x, y is the standard for GeoJSON and many formats
+		        coordinates: [
+		          lng,
+		          lat 
+		        ]
+		    },
+		    properties: {
+		        title: $("#form_title").val(),
+		        description: $("#form_desc").val() + "<img src='" + $('#blah').attr('src') + "'/>",
+                "marker-color":"#0099ff"
+		    }
+		}).addTo(map);
+   	}
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+});
 
--- js/slider.js
+++ js/slider.js
@@ -30,26 +30,29 @@
     $( "#slider" ).slider({
       value: 0,
       min: 0,
-      max: 4,
+      max: 9,
       step: 1,
       slide: function( event, ui ) {
         $("#cf7 img").removeClass("opaque");
 
         var newImage = ui.value;
         var oldImage = $("#slider").slider("value");
-        if (newImage == 4) {
+        if (newImage == 9) {
             $("#historical-maps").fadeToggle( "slow", function() {
                 $("#modern-maps").css('display','inline');
                 $("#container_of_slider").before($("#modern-maps"));
                 $("#container_of_slider").css('padding-top', '0px');
                 $( "#amount" ).val( "2015 A.D.(Now!)");
+                $("#blurb").show();
                 //$("#modern-maps").fadeToggle("slow", "linear");
             });
-        } else if (oldImage == 4) {
+        } else if (oldImage == 9) {
             $("#modern-maps").css('display', "none");
             $("#modern-maps").before($("#container_of_slider"));
-            $("#container_of_slider").css('padding-top', '120px');
+            $("#container_of_slider").css('padding-top', '140px');
             $("#historical-maps").fadeToggle( "slow");
+            $("#blurb").hide();
+
         }
         $("#cf7 img").eq(newImage).addClass("opaque");
 
@@ -58,24 +61,38 @@
         console.log("This is page " + ui.value);
         
         //This is where we struggled to change descriptions
-        $( "" ).hover(
-          function() {
-            $( this ).append( $( "<span> ***</span>" ) );
-          }, function() {
-            $( this ).find( "span:last" ).remove();
-          }
-        );
+
         $("#desc"+oldImage).hide()
         $("#desc"+newImage).show()
         console.log(oldImage);
-        console.log(newImage);        
+        console.log(newImage);   
+
+
+       
         
-        $( "#amount" ).val( ui.value * 50 + 1800 + "A.D. ");
+        if (newImage === 0) {
+
+          $("#amount").val(1300 + " A.D.")}
+        else if (newImage === 1){$("#amount").val(1573 + " A.D.")}
+        else if (newImage === 2){$("#amount").val(1700 + " A.D.")}
+        else if (newImage === 3){$("#amount").val(1861 + " A.D.")}
+        else if (newImage === 4){$("#amount").val(1900 + " A.D.")}
+        else if (newImage === 5){$("#amount").val(1938 + " A.D.")}
+        else if (newImage === 6){$("#amount").val(1952 + " A.D.")}
+        else if (newImage === 7){$("#amount").val(1980 + " A.D.")}
+        else if (newImage === 8){$("#amount").val(2005 + " A.D.")}
       }
     });
-    $( "#amount" ).val( $( "#slider" ).slider( "value" )  * 50 + 1800  + "A.D. ");
+    $( "#amount" ).val( 1300 + " A.D. ");
   });
-
+        //Hovers
+        $("#link01").hover(
+          function() {
+            $("#circle1").show()
+          }, function() {
+            $("#circle1").hide()
+          }
+        );      
            // L.featureLayer(
            // {
            //     // this feature is in the GeoJSON format: see geojson.org
